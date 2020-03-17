92c74e89379f62ef1f3e95471e3dd925a146f267
Shoaib Rahman <shoaibswe@gmail.com>
2020-03-17 17:44:42 +0600

    Some Code Eedited

--- MostUsedKWusingBOW.py
+++ MostUsedKWusingBOW.py
@@ -1,38 +1,27 @@
 # -*- coding: utf-8 -*-
 """
 Created on Tue Mar 10 21:50:23 2020
-
-author: NTI/Shoaib
+author: Shoaib
 """
-
 # -*- coding: utf-8 -*-
 """
 Spyder Editor
 
-This is a temporary script file.
 """
-
-import nltk
-import numpy as np
-
+#import numpy as np
 #import pandas as pd
 #from nltk.tokenize import word_tokenize as WordTokenizer
-
-#paragraph=pd.read_csv(r'F:\\twdata.txt', encoding='latin1');
-#paragraph=pd.read_csv('F:/data10032020.csv', sep=', ', delimiter=None, header='infer', names=None, index_col=None, usecols=None, squeeze=False, prefix=None, mangle_dupe_cols=True, dtype=None, engine=None, converters=None, true_values=None, false_values=None, skipinitialspace=False, skiprows=None, nrows=None, na_values=None, keep_default_na=True, na_filter=True, verbose=False, skip_blank_lines=True, parse_dates=False, infer_datetime_format=False, keep_date_col=False, date_parser=None, dayfirst=False, iterator=False, chunksize=None, compression='infer', thousands=None, decimal=b'.', lineterminator=None, quotechar='"', quoting=0, escapechar=None, comment=None, encoding=None, dialect=None, error_bad_lines=True, warn_bad_lines=True, skipfooter=0, doublequote=True, delim_whitespace=False, low_memory=True, memory_map=False, float_precision=None)
-#paragraph=pd.read_table('F://tst.txt')
-
-
+import nltk
 import codecs
-# Get the data from the file
+
 with open('F://d4.csv', 'rb') as fp:
   paragraph = fp.read()
 
-# The data is base64 encoded. Let's decode it.
+#Dcoding base64 data
 paragraph = codecs.decode(paragraph,'utf-8')
-#paragraph.columns=["text"]
 
-#cleaning text
+#Data preprocessed with Trifacta
+#Now cleaning Data
 import re
 from nltk.corpus import stopwords
 nltk.download('stopwords')
@@ -48,16 +37,16 @@ sentences = nltk.sent_tokenize(str(paragraph))
 
 corpus=[]
 for i in range(len(sentences)):
-        review = re.sub('[^a-zA-Z]', ' ', sentences[i])
-        review = review.lower()
-        review = review.split()
-        review = [wordnet.lemmatize(word) for word in review  if not word in stopwords.words('english')]
-        review = " ".join(review)
-        corpus.append(review)
-#        review = [ps.stem(word) for word in review  if not word in stopwords.words('english')]
+        data = re.sub('[^a-zA-Z]', ' ', sentences[i])
+        data = data.lower()
+        data = data.split()
+        data = [wordnet.lemmatize(word) for word in data  if not word in stopwords.words('english')]
+        data = " ".join(data)
+        corpus.append(data)
+#       data = [ps.stem(word) for word in data  if not word in stopwords.words('english')]
         
          
-# WCount
+# WordCount
 MajorIssue = {} 
 for data in corpus: 
 	words = nltk.word_tokenize(data) 
